cafca607377c126d460c8ce79f4eeb78e49acc2b
Maks <mavol_55@mail.ru>
2024-03-04 19:38:43 +0200

    add scripts : burger-menu, swiper-modal

---
+++ src/scripts/burger-menu.js
@@ -0,0 +1,40 @@
+(() => {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const burgerMenuLink = document.querySelectorAll('.burger-menu__link');
+  const btnBurgerActive = document.querySelectorAll('.nav__burger-btn-active');
+  const btnBurgerClose = document.querySelector('.nav__burger-btn-close');
+  const bgFixed = document.querySelector('.bg-fixed');
+
+  btnBurgerActive.forEach(el => el.addEventListener('click', openBurgerMenu))
+  btnBurgerClose.addEventListener('click', closeBurgerMenu)
+  burgerMenuLink.forEach(it => it.addEventListener('click', closeBurgerMenu))
+
+  function openBurgerMenu() {
+    document.body.style.overflow = 'hidden'
+    document.body.style.paddingRight = '17px'
+    bgFixed.style.right = '17px'
+
+    burgerMenu.classList.remove('fadeOut')
+    burgerMenu.firstElementChild.classList.remove('fadeOutRight')
+    burgerMenu.classList.add('fadeIn')
+    burgerMenu.firstElementChild.classList.add('fadeInRight')
+
+    burgerMenu.classList.add('active')
+  }
+  function closeBurgerMenu() {
+    document.body.style.overflow = 'initial'
+    document.body.style.paddingRight = '0'
+    bgFixed.style.right = '0'
+
+    burgerMenu.classList.remove('fadeIn')
+    burgerMenu.firstElementChild.classList.remove('fadeInRight')
+    burgerMenu.classList.add('fadeOut')
+    burgerMenu.firstElementChild.classList.add('fadeOutRight')
+
+    setTimeout(() => {
+      burgerMenu.classList.remove('active')
+    }, 400)
+  }
+
+
+})()
--- src/scripts/init-swiper.js
+++ src/scripts/init-swiper.js
@@ -1,11 +1,46 @@
 (() => {
-  const swiper = new Swiper('.swiper-gallery', {
+  const swiperGallery = new Swiper('.swiper-gallery', {
     slidesPerView: 'auto',
     loop: true,
     speed: 400,
     spaceBetween: 5,
     autoplay: {
       delay: 5000
+    },
+    on: {
+      click: activeModalGallery,
     }
   });
+  const swiperModalGallery = new Swiper('.swiper-modal-gallery', {
+    loop: true,
+    speed: 600,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  const modalGallery = document.querySelector('.swiper-modal-gallery')
+
+  function activeModalGallery(e) {
+    const numSlide = Number(e.clickedSlide.ariaLabel.split('/')[0].trim())
+    indexSlide = numSlide && numSlide - 1
+
+    modalGallery.classList.add('active')
+    swiperModalGallery.slideTo(indexSlide)
+  }
+
+  modalGallery.addEventListener('click', (e) => {
+    const clickOnImg = e.target.parentElement.classList.contains('swiper-slide')
+    const clickBtnPrev = e.target.closest('.swiper-button-prev')
+    const clickBtnNext = e.target.closest('.swiper-button-next')
+
+    if (!clickOnImg && !clickBtnPrev && !clickBtnNext) {
+      modalGallery.classList.remove('active')
+    }
+  })
 })()
